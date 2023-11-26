469905d5a561fe010bf313e1474f6bf4c6ff42e7
MwanaNteba Yohani <yohani.mwananteba@student.vinci.be>
2023-11-26 21:01:41 +0100

    "1.1 API : read (3)"

--- exercises/1.1/app.js
+++ exercises/1.1/app.js
@@ -3,8 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var filmsRouter = require('./routes/films');
 
 
@@ -14,9 +12,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/films', filmsRouter);
 
 module.exports = app;
