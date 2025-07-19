3f54a6758410078cf9b51dda7b774ab00467a20c
yohani-mwananteba-vinci <yohani.mwananteba@student.vinci.be>
2025-07-19 21:23:22 +0200

    correction: ex 1.2

--- exercises/1.2/app.ts
+++ exercises/1.2/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 import filmRouter from "./routes/films";
 
 const app = express();
-let reqCounter = 0;     // Compteur de requêtes
+let reqCounter = 0;     // Compteur de requêtes     //C: Aurait pu se trouver prêt du Middleware
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,9 +12,10 @@ app.use("/films", filmRouter);
 
 // Application-level middleware comptant le nbr de requête de type GET (S'éxecute à chaque requêtes)
 app.use((_req, _res, next) => {
-    if(_req.method === "GET")
+    if(_req.method === "GET"){
         reqCounter++;
-    console.log("GET Counter : " + reqCounter);
+        console.log("GET Counter : " + reqCounter); //C: OK mais il aurait fallu mettre ${reqCounter}
+    }
     next();
 });
 
