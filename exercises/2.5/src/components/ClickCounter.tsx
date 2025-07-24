bf677b824132d4b748c05fb1200ef02c3b05ff18
yohani-mwananteba-vinci <yohani.mwananteba@student.vinci.be>
2025-07-24 13:48:29 +0200

    correction: ex2.5

--- exercises/2.5/src/components/ClickCounter.tsx
+++ exercises/2.5/src/components/ClickCounter.tsx
@@ -2,32 +2,29 @@ import { useState } from "react";
 
 interface ClickCounterProps {
   title: string;
-  messageCounter: string;
-  messageButton: string;
+  messageCounter: string; //C: Aurait dû être une propriété optionnelle (rappel: "messageCounter ?: string");
+  messageButton: string; //C: Aurait dû être une propriété optionnelle
 }
 
 const ClickCounter = ({
   title,
-  messageCounter,
+  messageCounter, //C: Ok mais on pouvait lui donner une valeur par défaut pour les messages
   messageButton,
 }: ClickCounterProps) => {
   const [count, setCount] = useState(0);
   const [isOnButton, setIsOnButton] = useState(false);
 
+  // C: On aurait pû mettre les fonctions handle dans le return (on fait au plus libile)
   const handleCounter = () => {
-    // console.log("Number of click = " + count);
-    // if (count === 10) console.log("The messageCounter is update !");
     setCount((count) => count + 1);
   };
 
   const handleMessageEnter = () => {
-    // console.log("Mouse on the button !");
-    setIsOnButton(!isOnButton);
+    setIsOnButton(!isOnButton); // C: Pour les set avec des booleans, mieux vaut mettre setIsOnButton(true); => Evite la confusion
   };
 
   const handleMessageLeave = () => {
-    // console.log("Mouse left the button !");
-    setIsOnButton(!isOnButton);
+    setIsOnButton(!isOnButton); // C: Pour les set avec des booleans, mieux vaut mettre setIsOnButton(false); => Evite la confusion
   };
 
   return (
@@ -44,6 +41,7 @@ const ClickCounter = ({
       <p>{count >= 10 ? messageCounter : undefined}</p>
     </>
   );
+  //Remarque: Pour les messages: on pouvait mettre les balises DANS la variable de la propriété (ex: {count >= 10 ? <p>{on10ClickMessage}</p> : null})
 };
 
 export default ClickCounter;
